7cfb64448baf4295ecbfd728e86616ed36984912
tom <tom.g.woodward@gmail.com>
2019-09-05 12:40:53 -0400

    more books

--- src/config.books.js
+++ src/config.books.js
@@ -1,10 +1,28 @@
 const books = {
+  /* AP Physics */ '8d04a686-d5e8-4798-a27d-c608e4d0e187': {defaultVersion: '26.20'},
+  /* Am Gov 2e */ '9d8df601-4f12-4ac1-8224-b450bf739e5f': {defaultVersion: '5.9'},
   /* Anatomy & Physiology */ '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22': {defaultVersion: '16.7'},
+  /* Astronomy */ '2e737be8-ea65-48c3-aa0a-9f35b4c6a966': {defaultVersion: '21.8'},
   /* Biology 2e */ '8d50a0af-948b-4204-a71d-4826cba765b8': {defaultVersion: '15.47'},
   /* Biology for AP Courses */ '6c322e32-9fb0-4c4d-a1d7-20c95c5c7af2': {defaultVersion: '18.4'},
+  /* Business statistics */ 'b56bb9e9-5eb8-48ef-9939-88b1b12ce22f': {defaultVersion: '35.8'},
+  /* Calculus vol 1 */ '8b89d172-2927-466f-8661-01abc7ccdba4': {defaultVersion: '6.47' },
+  /* Calculus vol 2 */ '1d39a348-071f-4537-85b6-c98912458c3c': {defaultVersion: '5.21'},
+  /* Calculus vol 3 */ 'a31cd793-2162-4e9e-acb5-6e6bbd76a5fa': {defaultVersion: '5.30' },
   /* Chemistry 2e */ '7fccc9cf-9b71-44f6-800b-f9457fd64335': {defaultVersion: '9.18'},
   /* Chemistry: Atoms First 2e */ 'd9b85ee6-c57f-4861-8208-5ddf261e9c5f': {defaultVersion: '6.30'},
+  /* College Physics */ '031da8d3-b525-429c-80cf-6c8ed997733a': {defaultVersion: '17.22'},
   /* Concepts of Biology */ 'b3c1e1d2-839c-42b0-a314-e119a8aafbdd': {defaultVersion: '15.9'},
+  /* Intro statistics */ '30189442-6998-4686-ac05-ed152b91b9de': {defaultVersion: '23.33'},
+  /* Macro 2e */ '27f59064-990e-48f1-b604-5188b9086c29': {defaultVersion: '9.2'},
+  /* Macro 2e for AP */ '9117cf8c-a8a3-4875-8361-9cb0f1fc9362': {defaultVersion: '7.2'},
+  /* Micro 2e */ '5c09762c-b540-47d3-9541-dda1f44f16e5': {defaultVersion: '12.3'},
+  /* Micro 2e for AP */ '636cbfd9-4e37-4575-83ab-9dec9029ca4e': {defaultVersion: '7.3'},
+  /* Microbiology */ 'e42bd376-624b-4c0f-972f-e0c57998e765': {defaultVersion: '7.2'},
+  /* Principles of Economics 2e */ 'bc498e1f-efe9-43a0-8dea-d3569ad09a82': {defaultVersion: '7.3'},
+  /* University Physics vol 1 */ 'd50f6e32-0fda-46ef-a362-9bd36ca7c97d': {defaultVersion: '13.38'},
+  /* University Physics vol 2 */ '7a0f9770-1c44-4acd-9920-1cd9a99f2a1e': {defaultVersion: '16.39'},
+  /* University Physics vol 3 */ 'af275420-6050-4707-995c-57b9cc13c358': {defaultVersion: '12.18'},
 };
 
 module.exports = books;
