bd219ce0560637bd4d764b1a806d3bb5b00e4162
tom <tom.g.woodward@gmail.com>
2019-09-06 09:36:43 -0400

    sort books

--- src/config.books.js
+++ src/config.books.js
@@ -1,5 +1,4 @@
 const books = {
-  /* AP Physics */ '8d04a686-d5e8-4798-a27d-c608e4d0e187': {defaultVersion: '26.20'},
   /* Am Gov 2e */ '9d8df601-4f12-4ac1-8224-b450bf739e5f': {defaultVersion: '5.9'},
   /* Anatomy & Physiology */ '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22': {defaultVersion: '16.7'},
   /* Astronomy */ '2e737be8-ea65-48c3-aa0a-9f35b4c6a966': {defaultVersion: '21.8'},
@@ -12,6 +11,7 @@ const books = {
   /* Chemistry 2e */ '7fccc9cf-9b71-44f6-800b-f9457fd64335': {defaultVersion: '9.18'},
   /* Chemistry: Atoms First 2e */ 'd9b85ee6-c57f-4861-8208-5ddf261e9c5f': {defaultVersion: '6.30'},
   /* College Physics */ '031da8d3-b525-429c-80cf-6c8ed997733a': {defaultVersion: '17.22'},
+  /* College Physics for AP */ '8d04a686-d5e8-4798-a27d-c608e4d0e187': {defaultVersion: '26.20'},
   /* Concepts of Biology */ 'b3c1e1d2-839c-42b0-a314-e119a8aafbdd': {defaultVersion: '15.9'},
   /* Intro statistics */ '30189442-6998-4686-ac05-ed152b91b9de': {defaultVersion: '23.33'},
   /* Macro 2e */ '27f59064-990e-48f1-b604-5188b9086c29': {defaultVersion: '9.2'},
